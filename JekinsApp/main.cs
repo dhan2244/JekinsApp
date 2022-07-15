0b7ad2bc0b3fe5838fa6aa201d4714b6f8638840
dhan2244 <dhanushvenky123@gmail.com>
2022-07-15 15:32:20 +0530

    Math operation add

---
+++ JekinsApp/main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MyMaths;
+
+namespace JekinsApp
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Welcome to Jenkins");
+            //Console.WriteLine("Hello Dhanush");
+
+            Console.WriteLine("Welcome to my Console App 1.0");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Choose the option for operation");
+            Console.WriteLine("1. Add Two integers");
+            Console.WriteLine("2. Multiply Two Integers");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("--------------------------------");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Maths maths = new Maths();
+            int res = 0;
+            switch(choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the first number to be added");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the second number to be added");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    res = maths.Add(a, b);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the first number to be multiplied");
+                    int a1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the second number to be multiplied");
+                    int b1 = Convert.ToInt32(Console.ReadLine());
+                    res = maths.Multiply(a1, b1);
+                    break;
+                case 3:
+                    Console.WriteLine("Exited the App, Thank you :)");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+            Console.WriteLine("The result of your operation is " + res);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyMaths/Maths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyMaths
+{
+    public class Maths
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a,int b)
+        {
+            return a * b;
+        }
+    }
+}
